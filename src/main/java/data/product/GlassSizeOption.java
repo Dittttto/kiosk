95b8a31e25adaebc981723c00a66fb4e508bf684
DevFrog92 <yongsu1481@gmail.com>
2024-01-05 23:21:58 +0900

    feat: add GlassSizeOption enum

---
+++ src/main/java/data/product/GlassSizeOption.java
@@ -0,0 +1,26 @@
+package data.product;
+
+import java.math.BigDecimal;
+
+public enum GlassSizeOption implements Option {
+    LARGE("750ml", BigDecimal.valueOf(2000)),
+    MEDIUM("500ml", BigDecimal.ZERO);
+
+    private final String name;
+    private final BigDecimal addPrice;
+
+    GlassSizeOption(String name, BigDecimal addPrice) {
+        this.name = name;
+        this.addPrice = addPrice;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public BigDecimal getAddPrice() {
+        return addPrice;
+    }
+}
